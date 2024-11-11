5a5524abd63ed23f0af3540414316b27e7ddf2e6
hosenA51 <hosenahmed2552@gmail.com>
2024-11-11 19:03:59 +0600

    Create user using auth provider context

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const NavBar = () => {
+
+    const {name} = useContext(AuthContext);
+
+    console.log(name);
 
     const links = <>
         <li><NavLink to="/">Home</NavLink></li>
@@ -42,7 +47,7 @@ const NavBar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">{name}</a>
             </div>
         </div>
     );
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,37 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext } from 'react';
+import { auth } from '../firebase.init';
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const name = 'Elon Mask'
+
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        name,
+        createUser
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+
+/**
+ * 1. create context with null as default
+ * 2. create Provider
+ * 3. set a value with something (authInfo)
+ * 4. [ attention Please !!!!]
+ * 5. use the authProvider in the main.jsx
+ * 6. access the children inside the authProvider in the main.jsx
+ * 7. export AuthContext
+ */
